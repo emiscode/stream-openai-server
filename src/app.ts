e7fa1a565ba1bce29ee9019a72fcca891d78e2f7
Emilio Scodeler <emilio.scodeler@solides.com.br>
2023-12-21 12:45:36 -0300

    add stream-audio

--- src/app.ts
+++ src/app.ts
@@ -107,4 +107,72 @@ app.post('/audio', async (req: Request, res: Response) => {
   res.end();
 });
 
+app.post('/stream-audio', async (req: Request, res: Response) => {
+  const payload = req.body;
+
+  const prompt =
+    payload.prompt ||
+    'Teach me about the movement of the electron around the nucleus in a simple way with few words.';
+
+  const messages = [
+    {
+      role: 'user',
+      content: 'You are a remarkable quantum mechanics professor.',
+    },
+    {
+      role: 'assistant',
+      content:
+        'I am thrilled to teach you about the true nature of the universe!',
+    },
+    {
+      role: 'user',
+      content: `${prompt}.
+          If you receive a question not related to quantum mechanics or physics,
+          then you should advise about your abilities to only answer questions related to quantum mechanics
+          and then ask for a new question related to this topic.`,
+    },
+  ] as OpenAI.Chat.ChatCompletionMessage[];
+
+  const stream = await openai.chat.completions.create({
+    model: 'gpt-4',
+    max_tokens: 2000,
+    messages,
+    stream: true,
+  });
+
+  // Set the headers to enable SSE
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+
+  const chunks = [];
+
+  for await (const chunk of stream) {
+    const content = chunk.choices[0]?.delta?.content || '';
+    chunks.push(content);
+  }
+
+  const audio = await openai.audio.speech.create({
+    model: 'tts-1',
+    voice: 'alloy',
+    input: chunks.join(' '),
+  });
+
+  const buffer = Buffer.from(await audio.arrayBuffer());
+
+  // Convert the audio data to a Base64 string
+  const base64String = (buffer as Buffer).toString('base64');
+
+  // Set the headers to enable SSE
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+
+  // Send the Base64 string as an SSE message
+  res.write(base64String);
+
+  // End the SSE stream
+  res.end();
+});
+
 export default app;
